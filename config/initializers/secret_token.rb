c4fd662360a8020f1959c8393957e7b9181eb851
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-08-21 18:07:17 -0700

    initial commit - fresh rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RalleyRampage::Application.config.secret_key_base = '2766cc3df0b703aeca02257a7bc2de63ef650d9b4144b820074a92ccb405e00f3cf377ccb182db72d8b32ee3f61ed264cca2069f72c68e791a8cc30b72c45547'
