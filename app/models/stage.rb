43c662b6dbc07e74c9fd1a940ca7f671bb2d7aa1
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-08-21 21:50:08 -0700

    Added relationships to AR models

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -1,2 +1,4 @@
 class Stage < ActiveRecord::Base
+  has_many :team_stages
+  has_many :teams, :through => :team_stages
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,6 @@
 class Team < ActiveRecord::Base
+  has_many :team_challenges
+  has_many :challenges, :through => :team_challenges
+  has_many :team_stages
+  has_many :stages, :through => :team_stages
 end
