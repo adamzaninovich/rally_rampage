41b12f799847ebdd50b7ce20d7d6a9f237e44a70
Tyler Clemens <tyler.r.clemens@gmail.com>
2013-08-22 13:31:23 -0700

    finished team seed data and removed last names

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,24 +9,45 @@
 Team.delete_all
 teams = [{
   name: 'Dangerzone',
-  driver: 'Nathaniel Barnes',
-  navigator: 'Holly Barnes',
+  driver: 'Nathaniel',
+  navigator: 'Holly',
   vehicle: '2001 Pontiac Aztek',
-  pax: ''
+  pax: '0.866'
 },
 {
   name: 'Strazzerinovich',
-  driver: 'Adam Zaninovich',
-  navigator: 'Elicia Strazzeri',
+  driver: 'Adam',
+  navigator: 'Elicia',
   vehicle: '2011 Ford Mustang V6 Coupe',
-  pax: ''
+  pax: '0.826'
 },
 {
   name: "Let's Get Frosty",
-  driver: 'Tyler Clemens',
-  navigator: 'Lauren Oas',
+  driver: 'Tyler',
+  navigator: 'Lauren',
   vehicle: '2006 Ford Focus ZX4',
-  pax: ''
+  pax: '0.804'
+},
+{
+  name: "Katia's Epic Win",
+  driver: 'Alex',
+  navigator: 'Katia',
+  vehicle: '2013 Honda Fit',
+  pax: '0.795'
+},
+{
+  name: "Anarchy",
+  driver: 'Myron',
+  navigator: 'Shalyn',
+  vehicle: '2008 Lexus IS250',
+  pax: '0.826'
+},
+{
+  name: "Amish",
+  driver: 'Jared',
+  navigator: 'Caitlin',
+  vehicle: '2012 Hyundai Sonata GL',
+  pax: '0.827'
 }]
 teams.each do |team|
   Team.create! team
