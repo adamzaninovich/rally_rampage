46e6ca9209a069183d241100ca5c9352ccc831d9
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-08-30 23:09:55 -0700

    logic for current stage route

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -11,6 +11,10 @@ class StagesController < ApplicationController
   # GET /stages/current
   def current
     @stage = Stage.current
-    render :show
+    if @stage
+      render :show
+    else
+      redirect_to results_path # todo: needs a results model and controller, no AR needed probably
+    end
   end
 end
