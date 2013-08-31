6812dc986e2aebb2e4f614d6426943645ea5a5b7
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-08-30 23:06:35 -0700

    adds Stage.current

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -5,14 +5,9 @@ class Stage < ActiveRecord::Base
   TYPES = %w[ideal_time speed odometer]
   validates_inclusion_of :stage_type, in: Stage::TYPES
 
-  # I spiked this to make sure that we
-  # didn't need to add a 'finished'
-  # column in the table, but we should
-  # probably TDD this shit - Adam
-
-  #def self.current
-    #Stage.first
-  #end
+  def self.current
+    Stage.order(:order_number).reject(&:finished?).first
+  end
 
   def finished?
     stage_results.any? && !stage_results.map(&:finished?).include?(false)
--- spec/models/stage_spec.rb
+++ spec/models/stage_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe Stage do
-  let(:result) { StageResult.create stage: stage, team: Team.create }
-  let(:stage) { Stage.create stage_type: 'speed' }
-  subject { stage }
 
   describe '#finished?' do
+    let(:result) { StageResult.create stage: stage, team: Team.create }
+    let(:stage) { Stage.create stage_type: 'speed' }
+    subject { stage }
+
     context "when all stage_results are finished" do
       before { result.start! and result.finish! }
       it { should be_finished }
@@ -20,6 +21,39 @@ describe Stage do
     context "when no stage_results are finished" do
       before { result.start! }
       it { should_not be_finished }
+    end
+  end
+
+  describe '.current' do
+    let!(:stages) do
+      team = Team.create
+      %w[ideal_time odometer].each_with_index.map do |stage_type, i|
+        stage = Stage.create stage_type: stage_type, order_number: i+1
+        StageResult.create stage: stage, team: team
+        stage
+      end
+    end
+    context "when no stages have been started" do
+      it "returns the first stage" do
+        Stage.current.order_number.should == 1
+      end
+    end
+    context "when the first stage is finished and the second has not been started" do
+      it "returns the second stage" do
+        stage = stages.first
+        stage.stage_results.first.start!
+        stage.stage_results.first.finish!
+        Stage.current.order_number.should == 2
+      end
+    end
+    context "when both stages are finished" do
+      it "returns nil" do
+        stages.each do |stage|
+          stage.stage_results.first.start!
+          stage.stage_results.first.finish!
+        end
+        Stage.current.should be_nil
+      end
     end
   end
 
