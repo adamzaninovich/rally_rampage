947a589f2c1bf933aa9abc91a5e546f2005d5b04
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-10-26 18:34:33 -0700

    adds api v1 route and inflection to support 'API'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 RallyRampage::Application.routes.draw do
 
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :teams, only: [:index, :show]
+    end
+  end
+
   get 'stages/current' => 'stages#current', as: :current_stage
   resources :stages, only: [:index, :show] do
     post 'start'
